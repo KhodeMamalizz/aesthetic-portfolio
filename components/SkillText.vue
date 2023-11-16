ffdfc590e50c3a119c9ff8e693a1d3dff93f33f8
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2023-11-17 00:33:27 +0330

    skills section done

---
+++ components/SkillText.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+    import { slideInFromRight, slideInFromLeft } from '../utils/motion'
+    import { SparklesIcon } from "@heroicons/vue/20/solid"
+</script>
+
+<template>
+    <div class="w-full h-full flex flex-col justify-center items-center">
+        <div 
+            v-motion-slide-top
+            class="Welcome-box py-[1rem] px-[1.4rem] border border-[#7042f88b] opacity-[.9]"
+        >
+            <SparklesIcon class="text-[#b49bff] mr-[10px] w-5 h-5" />
+            <h1 class="Welcome-text text-[13px]">
+                Modern Technologies i use   
+            </h1>
+        </div>
+        <div 
+            v-motion="slideInFromLeft(500)"
+            class="text-[30px] text-white font-medium mt-10 text-center mb-[1rem]"
+        >
+            Making Websites, Web apps, Mobile apps
+        </div>
+        <div 
+            v-motion="slideInFromRight(500)"
+            class="cursive text-[20px] text-gray-200 my-10 text-center"
+        >
+            Also know how to center a div LOL
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- components/Skills.vue
+++ components/Skills.vue
@@ -1,7 +1,24 @@
 
 import type { Skills } from '#build/components';
 <script setup lang="ts">
-    import { skills } from '@constants'
+    import { skills, frontendSkills, backendSkills, fullstackSkills, otherSkills } from '@constants'
+    const {$gsap: gsap} = useNuxtApp()
+
+    onMounted(() => {
+        gsap
+            .from(
+                '.skills',
+                {
+                    opacity: 0,
+                    duration: 2,
+                    stagger: 0.1,
+                    scrollTrigger: {
+                        trigger: '#skills',
+                        start: 'top bottom',
+                    }
+                }
+            )
+    })
 </script>
 
 <template>
@@ -9,14 +26,62 @@ import type { Skills } from '#build/components';
         id="skills"
         class="flex flex-col items-center justify-center gap-3 h-full overflow-hidden py-20"
     >
+        <SkillText />
         <div class="flex justify-around flex-wrap mt-4 gap-5 items-center">
             <img 
-                v-for="(skill, index) in skills" 
+                class="skills"
+                v-for="(skill, index) in frontendSkills" 
                 :key="index"
                 :width="skill.width"
                 :height="skill.height"
                 :src="skill.src" 
                 :alt="skill.skill_name">
+        </div>
+        <div class="flex justify-around flex-wrap mt-4 gap-5 items-center">
+            <img 
+                class="skills"
+                v-for="(skill, index) in backendSkills" 
+                :key="index"
+                :width="skill.width"
+                :height="skill.height"
+                :src="skill.src" 
+                :alt="skill.skill_name">
+        </div>
+        <div class="flex justify-around flex-wrap mt-4 gap-5 items-center">
+            <img 
+                class="skills"
+                v-for="(skill, index) in fullstackSkills" 
+                :key="index"
+                :width="skill.width"
+                :height="skill.height"
+                :src="skill.src" 
+                :alt="skill.skill_name">
+        </div>
+        <div class="flex justify-around flex-wrap mt-4 gap-5 items-center">
+            <img 
+                class="skills"
+                v-for="(skill, index) in otherSkills" 
+                :key="index"
+                :width="skill.width"
+                :height="skill.height"
+                :src="skill.src" 
+                :alt="skill.skill_name">
+        </div>
+
+        <div class="w-full h-full absolute">
+            <div class="w-full h-full z-[-10] opacity-30 absolute flex justify-center items-center bg-cover">
+                <video 
+                    class="w-full h-auto"
+                    preload="false"
+                    playsInline
+                    muted
+                    loop
+                    autoplay
+                    src="/cards-video.webm"
+                >
+
+                </video>
+            </div>
         </div>
     </section>
 </template>
